89814c9dedd6d42b11e981618ca0c365f0e315c3
D.Naydenov <naidim@abv.bg>
2018-07-03 02:03:42 +0300

    Removed Semantic UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 import LoginForm from './components/LoginForm';
 import Home from './components/Home'
 import { BrowserRouter, Route, Redirect } from 'react-router-dom'
